436040363e3ca50f71e2fa1361ec1114eff3d220
habib rouatbi <habib.rouatbi123@gmail.com>
2025-03-29 00:41:12 +0100

    | 🦉 Feat  : added SmsPagerAdapter.kt

---
+++ app/src/main/java/com/example/sms_scheduler/ui/home/SmsPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.sms_scheduler.ui.home
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SmsPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> PendingSmsFragment()
+            1 -> PendingSmsFragment()
+            2 -> PendingSmsFragment()
+            else -> throw IllegalArgumentException("Invalid tab position")
+        }
+    }}
