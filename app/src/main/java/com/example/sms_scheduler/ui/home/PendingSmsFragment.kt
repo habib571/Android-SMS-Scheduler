86eb6fc75fbfffd1c143643b8bf1e5089700d657
habib rouatbi <habib.rouatbi123@gmail.com>
2025-03-29 00:40:26 +0100

    | 🦉 Feat  : added PendingSmsFragment.kt

---
+++ app/src/main/java/com/example/sms_scheduler/ui/home/PendingSmsFragment.kt
@@ -0,0 +1,31 @@
+package com.example.sms_scheduler.ui.home
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sms_scheduler.R
+
+class PendingSmsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PendingSmsFragment()
+    }
+
+    private val viewModel: PendingSmsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_pending_sms, container, false)
+    }
+}
