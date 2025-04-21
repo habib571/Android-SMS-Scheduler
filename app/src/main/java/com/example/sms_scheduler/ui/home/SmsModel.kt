85b01b4facac17ddea41f6f7794795ad7e4e2c4c
habib rouatbi <habib.rouatbi123@gmail.com>
2025-04-21 12:10:00 +0200

    ♻️ Refactor : SmsModel

--- app/src/main/java/com/example/sms_scheduler/ui/home/SmsModel.kt
+++ app/src/main/java/com/example/sms_scheduler/ui/home/SmsModel.kt
@@ -1,8 +1,17 @@
 package com.example.sms_scheduler.ui.home
 
+
 data class SmsModel(
     val id: Int,
     val content: String,
     val sentTo: String,
-    val dateTime: String
+    val dateTime: String ,
+    val status: SmsStatus
+
 )
+
+enum class SmsStatus{
+    PENDING ,
+    SENT ,
+    CANCELLED
+}
